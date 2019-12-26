52e8538606837b1fbd58695135006036276c4bd0
Patrick Germann <30571074+patrickgermann@users.noreply.github.com>
2019-12-26 12:32:56 +0100

    create GET requests

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const app = express();
 
-// simple GET request for /greetings
-app.get('/greetings', (req, res) => {
-    res.json({greeting: 'hello World!'});
+// Send a GET request to /quotes to READ a list of quotes
+app.get('/quotes', (req, res) => {
+    res.json({data});
 });
 
-// Send a GET request to /quotes to READ a list of quotes
 // Send a GET request to /quotes/:id to READ (view) a quote
+app.get('/quotes/:id', (req, res) => {
+     const quote = data.quotes.find(quote => quote.id == req.params.id);
+     res.json(quote);
+});
 // Send a POST request to /quotes to CREATE a new quote
 // Send a PUT request to /quotes/:id to UPDATE (edit) a quote
 // Send a DELETE request to /quotes/:id to DELETE a quote
