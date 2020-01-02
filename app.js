e39ec95f3dbf3357c0fae7c132ade2075c70aacc
Patrick Germann <30571074+patrickgermann@users.noreply.github.com>
2020-01-02 15:40:11 +0100

    refactoring to helper function to avoid try/catch in routes

--- app.js
+++ app.js
@@ -4,54 +4,52 @@ const app = express();
 // import records.js for db methods 
 const records = require('./records');
 
+// Helper function to remove try catch blocks in routes
+function asyncHandler(cb){
+    return async (req, res, next)=>{
+      try {
+        await cb (req, res, next);
+      } catch(err){
+        next(err);
+      }
+    };
+  }
+
 app.use(express.json());
 
 // Send a GET request to /quotes to READ a list of quotes
-app.get('/quotes', async (req, res) => {
-    try {
+app.get('/quotes', asyncHandler( async (req, res) => {
         const quotes = await records.getQuotes();
         res.json(quotes);
-    } catch(err) {
-        res.json({message: err.message});
-    }
-
-});
+}));
 
 // Send a GET request to /quotes/:id to READ (view) a quote
-app.get('/quotes/:id', async (req, res) => {
-    try {
+app.get('/quotes/:id', asyncHandler( async (req, res) => {
         const quote = await records.getQuote(req.params.id);
         if (quote) {
             res.json(quote);
         } else {
             res.status(404).json({message: "Quote not found!"});
         }
-        
-    } catch(err) {
-        res.status(500).json({message: err.message});
-    }
-});
-// Send a POST request to /quotes to CREATE a new quote
-app.post('/quotes', async (req,res) => {
-    try {
-        if (req.body.author && records.body.quote) {
-            const quote = await records.createQuote({
-                quote: req.body.quote,
-                author: req.body.author
-            });
-            res.status(201).json(quote);      
-        } else {
-            res.status(400).json({message: "Quote and author required."});
-        }
-    } catch(err) {
-        res.status(500).json({message: err.message});
-    }
+}));
 
-});
+// Send a POST request to /quotes to CREATE a new quote
+app.post('/quotes', asyncHandler( async (req, res) => {
+    if (req.body.author && req.body.quote) {
+        const quote = await records.createQuote({
+            quote: req.body.quote,
+            author: req.body.author
+        });
+        res.status(201).json(quote);      
+    } else {
+        res.status(400).json({message: "Quote and author required."});
+    }
+}));
+
+
 // Send a PUT request to /quotes/:id to UPDATE (edit) a quote
-app.put('/quotes/:id', async (req,res) => {
-    try {
-        const quote = await records.getQuote(req.params.id);
+app.put('/quotes/:id', asyncHandler( async (req,res) => {
+    const quote = await records.getQuote(req.params.id);
         if (quote) {
             quote.quote = req.body.quote;
             quote.author = req.body.author;
@@ -62,25 +60,36 @@ app.put('/quotes/:id', async (req,res) => {
         } else {
             res.status(404).json({message: "Quote not found!"});
         }
+}));
 
-    } catch(err) {
-        res.status(500).json({message: err.message});
-    }
-});
 // Send a DELETE request to /quotes/:id to DELETE a quote
-app.delete('/quotes/:id', async (req,res) => {
-    try {
-        const quote = await records.getQuote(req.params.id);
-        if (quote){
-            await records.deleteQuote(quote);
-            res.status(204).end();
-        } else {
-            res.status(404).json({message: "Quote not found!"});
-        }
-    } catch(err) {
-        res.status(500).json({message: err.message});
+app.delete('/quotes/:id', asyncHandler( async (req,res, next) => {
+    const quote = await records.getQuote(req.params.id);
+    if (quote){
+        await records.deleteQuote(quote);
+        res.status(204).end();
+    } else {
+        res.status(404).json({message: "Quote not found!"});
     }
-});
+}));
+
 // Send a GET request to /quotes/quote/random to READ (view) a random quote
+
+// Middleware, when no route has been found
+app.use( (req, res, next) => {
+    const err = new Error('Not found');
+    err.status = 404;
+    next(err);
+});
+
+// Error handler (used by Middleware)
+app.use( (err, req, res, next) => {
+    res.status(err.status || 500);
+    res.json({
+        error: {
+            message: err.message
+        }
+    });
+});
 
 app.listen(3000, () => console.log('Quote API listening on port 3000!'));
