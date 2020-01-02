036b1b7a0851d7ff17c10c5e417439364432beb7
Patrick Germann <30571074+patrickgermann@users.noreply.github.com>
2020-01-02 15:52:50 +0100

    refactoring routes  for /api

--- app.js
+++ app.js
@@ -1,79 +1,11 @@
 const express = require('express');
 const app = express();
-
-// import records.js for db methods 
-const records = require('./records');
-
-// Helper function to remove try catch blocks in routes
-function asyncHandler(cb){
-    return async (req, res, next)=>{
-      try {
-        await cb (req, res, next);
-      } catch(err){
-        next(err);
-      }
-    };
-  }
+const routes = require('./routes');
 
 app.use(express.json());
 
-// Send a GET request to /quotes to READ a list of quotes
-app.get('/quotes', asyncHandler( async (req, res) => {
-        const quotes = await records.getQuotes();
-        res.json(quotes);
-}));
-
-// Send a GET request to /quotes/:id to READ (view) a quote
-app.get('/quotes/:id', asyncHandler( async (req, res) => {
-        const quote = await records.getQuote(req.params.id);
-        if (quote) {
-            res.json(quote);
-        } else {
-            res.status(404).json({message: "Quote not found!"});
-        }
-}));
-
-// Send a POST request to /quotes to CREATE a new quote
-app.post('/quotes', asyncHandler( async (req, res) => {
-    if (req.body.author && req.body.quote) {
-        const quote = await records.createQuote({
-            quote: req.body.quote,
-            author: req.body.author
-        });
-        res.status(201).json(quote);      
-    } else {
-        res.status(400).json({message: "Quote and author required."});
-    }
-}));
-
-
-// Send a PUT request to /quotes/:id to UPDATE (edit) a quote
-app.put('/quotes/:id', asyncHandler( async (req,res) => {
-    const quote = await records.getQuote(req.params.id);
-        if (quote) {
-            quote.quote = req.body.quote;
-            quote.author = req.body.author;
- 
-            await records.updateQuote(quote);
-            res.status(204).end();
-
-        } else {
-            res.status(404).json({message: "Quote not found!"});
-        }
-}));
-
-// Send a DELETE request to /quotes/:id to DELETE a quote
-app.delete('/quotes/:id', asyncHandler( async (req,res, next) => {
-    const quote = await records.getQuote(req.params.id);
-    if (quote){
-        await records.deleteQuote(quote);
-        res.status(204).end();
-    } else {
-        res.status(404).json({message: "Quote not found!"});
-    }
-}));
-
-// Send a GET request to /quotes/quote/random to READ (view) a random quote
+//if route begins with /api, use the routes.js
+app.use('/api', routes);
 
 // Middleware, when no route has been found
 app.use( (req, res, next) => {
